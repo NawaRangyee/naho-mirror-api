0c4005d40d4abfdc7955d01545d66fd28dcdf0ec
Cellularhacker <wva3cdae@gmail.com>
2022-10-23 12:04:09 +0900

    Update

--- util/sendMessage.go
+++ util/sendMessage.go
@@ -10,7 +10,7 @@ import (
 
 func SendFailed(location string, err error) {
 	at := time.Now().In(config.Loc)
-	msg := fmt.Sprintf("[ERROR/%s]\n=> %s", location, err)
+	msg := fmt.Sprintf("<%s>[ERROR/%s]\n=> %s", config.GetServiceFullName(), location, err)
 	logger.L.Errorw(err.Error(), "location", location)
 
 	telegram.SendMessageAt(msg, at)
@@ -18,28 +18,28 @@ func SendFailed(location string, err error) {
 
 func SendNotice(header, location, content string) {
 	at := time.Now().In(config.Loc)
-	msg := fmt.Sprintf("[%s/%s]\n=> %s\n", header, location, content)
+	msg := fmt.Sprintf("<%s>[%s/%s]\n=> %s\n", config.GetServiceFullName(), header, location, content)
 	logger.L.Infow(content, "header", header, "location", location)
 
 	telegram.SendMessageAt(msg, at)
 }
 
 func SendStarted(hostname string, localIP string, pubIP string) {
-	msg := fmt.Sprintf("Server started successfully\nHostname:%s\nLocal IP:%s\nPublic IP:%s", hostname, localIP, pubIP)
+	msg := fmt.Sprintf("<%s>Server started successfully\nHostname:%s\nLocal IP:%s\nPublic IP:%s", config.GetServiceFullName(), hostname, localIP, pubIP)
 	logger.L.Infow(msg, "func", "SendStarted()", "hostname", hostname, "localIP", localIP, "pubIP", pubIP)
 
 	telegram.SendMessage(msg)
 }
 
 func SendStopped(hostname string, localIP, pubIP string) {
-	msg := fmt.Sprintf("Server stopping normally\nHostname:%s\nLocal IP:%s\nPublic IP:%s", hostname, localIP, pubIP)
+	msg := fmt.Sprintf("<%s>Server stopping normally\nHostname:%s\nLocal IP:%s\nPublic IP:%s", config.GetServiceFullName(), hostname, localIP, pubIP)
 	logger.L.Infow(msg, "func", "SendStopped()", "hostname", hostname, "localIP", localIP, "pubIP", pubIP)
 
 	telegram.SendMessage(msg)
 }
 
 func SendForcedStopped(hostname string, localIP, pubIP string) {
-	msg := fmt.Sprintf("Server stopping forcingly\nHostname:%s\nLocal IP:%s\nPublic IP:%s", hostname, localIP, pubIP)
+	msg := fmt.Sprintf("<%s>Server stopping forcingly\nHostname:%s\nLocal IP:%s\nPublic IP:%s", config.GetServiceFullName(), hostname, localIP, pubIP)
 	logger.L.Warnw(msg, "func", "SendForcedStopped()", "hostname", hostname, "localIP", localIP, "pubIP", pubIP)
 
 	telegram.SendMessage(msg)
